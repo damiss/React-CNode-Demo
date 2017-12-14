9465a7d3f9f2276887ceb1358d65ce1144ab1790
王军-PC\王军 <296188842@qq.com>
2017-12-14 17:36:19 +0800

    login-pending...

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { InputItem, Button, Flex, Card, Icon } from 'antd-mobile';
 import axios from './../../axios/index';
 import Default from './../Main/Default';
+import dB from './../../utils/index'
 
 export default class Login extends Component {
     constructor(props) {
@@ -16,9 +17,19 @@ export default class Login extends Component {
         this.props.history.push('/');
     }
     handleLogin() {
-        axios.post(`/accesstoken`, { accesstoken: "2ff3f772-f90d-4c02-8279-70ea42be59eb"})
+        axios.post(`/accesstoken`, { accesstoken: this.state.value})
             .then(res => {
                 console.log(res)
+                if(res.status == "200"){
+                    dB.save(["success_token"], { "success_token": this.state.value});
+                    dB.save(["avatar_url", "id", "loginname"], { "avatar_url": res.data.avatar_url, "id": res.data.id, "loginname": res.data.loginname })
+                }
+                if (dB.get("whichTab") == "redTab") {
+                    console.log(123)
+                    this.props.history.push('/write');
+                }else {
+                    console.log(222)
+                }
             })
             .catch(function (error) {
                 console.log(error);
@@ -33,7 +44,7 @@ export default class Login extends Component {
             <Card direction="column" style={{ height: '100%', width: '100%' }}>
                 <Flex direction="column" style={{ padding: '0.5rem 0' }}>
                     <span>登录</span>
-                    <InputItem style={{ width: '95%', padding: '0.5rem 0 0 0' }} placeholder="请输入access_token" value={this.state.value} onChange={this.handleChange.bind(this)} />
+                    <input type="text" style={{ width: '95%', padding: '0.5rem 0 0 0' }} placeholder="请输入access_token" value={this.state.value} onChange={this.handleChange.bind(this)} />
                     <Button
                         type="primary"
                         style={{ width: '95%', marginTop: '0.5rem' }}
